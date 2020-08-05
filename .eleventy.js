3ea0433daad045f3f595872c3356bb0eb906c347
Mike Rosack <mrosack@gmail.com>
2020-08-05 11:19:20 -0500

    add tailwind/sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addFilter('htmlDateString', dateObj => {
+        return DateTime.fromJSDate(dateObj).toFormat('MMMM d, yyyy')
+    });
+
     return {
       passthroughFileCopy: true,
       dir: {
         input: 'src',
         includes: '_includes',
         data: '_data',
-        output: '_site',
+        output: 'dist',
       },
     };
   };
