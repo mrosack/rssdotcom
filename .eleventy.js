e6cd1fc62ac0edd228a2003b0edd15a0f1583109
Mike Rosack <mrosack@gmail.com>
2020-08-09 07:18:33 -0500

    i am gud at see ess ess

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,32 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/assets/img");
 
+  let markdownLib = require("markdown-it")({
+    html: true
+  })
+  .use(require("markdown-it-attrs"))
+  .use(require('markdown-it-container'), 'section', {
+    render: (tokens, idx) => {
+      var m = tokens[idx].info.trim().match(/^section\s+(.*)$/);
+   
+      if (tokens[idx].nesting === 1) {
+        // opening tag
+        return `<section${m && m[1] ? ' class="' + m[1] + '"' : ''}><content>`;
+   
+      } else {
+        // closing tag
+        return '</content></section>\n';
+      }
+    }
+  })
+  .use(require('markdown-it-container'), 'split', {
+    render: (tokens, idx) => {
+      return tokens[idx].nesting === 1 ? '<split>' : '</split>';
+    }
+  });
+  
+  eleventyConfig.setLibrary("md", markdownLib);
+
   return {
     passthroughFileCopy: true,
     dir: {
--- src/index.md
+++ src/index.md
@@ -1,4 +1,20 @@
 ---
 layout: mainlayout.njk
 ---
+::::: section bg-blue1
 # Rosack Software Solutions, LLC
+
+Rosack Software Solutions is a software development consulting firm headquartered in Greenville, WI founded by Mike Rosack. We specialize in full stack development, and we’d love to help make your software dreams into reality.
+:::: split
+::: section
+## Architecture / Cloud Infrastructure
+
+Need help designing a solution? Not sure what technologies to use? Overwhelmed by all the options out in the cloud? Let us help you create the blueprints for your idea.
+:::
+::: section
+## Development
+
+Need an experienced developer to assist your team in a crunch? We have a broad range of experience in many technologies, and it’s likely we’ll be able to step up to the plate and hit a home run for you.
+:::
+::::
+:::::
